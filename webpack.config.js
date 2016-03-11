795554e1b9d10a84a8b95d915e1c9536072ce1f4
xiaohuazheng <463027542@qq.com>
2016-03-11 14:00:09 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,74 @@ var output = {
     filename: 'navheader.min.js'
 }
 module.exports = {
-  resolveLoader: { root: path.join(__dirname, "node_modules") },
+    resolveLoader: { root: path.join(__dirname, "node_modules") },
+    context: __dirname + path.sep + "app",//path.sep系统分隔符
+    /**
+     * 入口模块配置
+     * 默认每个入口包分别声明着内部需要依赖哪些模块
+     * webpack会在打包阶段合并这些依赖模块
+     * 但是通过CommonsChunkPlugin可以声明某个模块里的所有
+     * 依赖为公用模块,既其它的入口包在依赖common模块里的包时,
+     * 是不会重复打包的
+     */
+    entry: './public/javascripts/js/navheader.js',
+    output: output,
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'jsx-loader'},
+            { test: /\.css$/, loader: "style-loader!css-loader"},
+            { test: /\.png$/, loader: "url-loader?mimetype=image/png" }
+        ]
+    },
+    resolve: {
+        /*简称配置*/
+        alias: {
+            navheader : __dirname + '/public/javascripts/js/navheader.js',
+            crawler : __dirname + '/public/javascripts/js/crawler.js',
+            dealcoll : __dirname + '/public/javascripts/js/dealcoll.js',
+            loginreg : __dirname + '/public/javascripts/js/loginreg.js',
+            myCharts : __dirname + '/public/javascripts/js/myCharts.js'
+        }
+    },
+    plugins: [
+        /*
+         * desc [CommonsChunkPlugin]
+         * 公用模块配置,入口模块的common为公用模块,
+         * 当其它模块引用common中的模块后是不会重复打包的
+         */
+        // new webpack.optimize.CommonsChunkPlugin('common', 'commons.chunk.js'),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        (function() {
+            if (isProduction) {
+                return  new webpack.optimize.UglifyJsPlugin({
+                    sourceMap: false
+                });
+            }
+            return function() {};
+        })(),
+        function() {
+            this.plugin("done", function(stats) {
+                var ejs = require('ejs'),
+                    fs = require("fs");
+                fs.writeFileSync(
+                    path.join(__dirname, "javascripts/js"),
+                    JSON.stringify(stats.toJson().assetsByChunkName)
+                );
 
-  entry: './public/javascripts/js/navheader.js',
-  output: output
+                var cache = require(path.join(__dirname, "javascripts/js"));
+                console.info(cache);
+                ejs.renderFile(path.join(__dirname, "javascripts/js"),cache,function (err,html) {
+                  if (err) {
+                    console.info(err);
+                  } else {
+                    fs.writeFileSync(
+                        path.join(__dirname, "javascripts/js"),
+                        html
+                    )
+                  }
+                });
+            });
+        }
+    ]
 };

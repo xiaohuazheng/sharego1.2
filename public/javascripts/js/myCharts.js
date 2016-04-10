a5f5342fc356e6f6bbe6dec2e292ec6553df5fe3
xiaohuazheng <463027542@qq.com>
2016-04-10 14:01:51 +0800

    bar chart

--- public/javascripts/js/myCharts.js
+++ public/javascripts/js/myCharts.js
@@ -1,3 +1,4 @@
+
 var data = {
 	labels : ["好评率","正品","质量不错","没有色差","穿着舒适","服务满意","物流快"],
 	datasets : [
@@ -11,13 +12,43 @@ var data = {
 		}
 	]
 }
+var data1 = {
+	labels : ["好评率(标准/实际)","正品(标准/实际)","质量不错(标准/实际)","没有色差(标准/实际)","穿着舒适(标准/实际)","服务满意(标准/实际)","物流快(标准/实际)"],
+	datasets : [
+		{
+			fillColor : "rgba(220,220,220,0.5)",
+			strokeColor : "rgba(220,220,220,1)",
+			data : [100,100,100,100,100,100,100]
+		},
+		{
+			fillColor : "rgba(151,187,205,0.5)",
+			strokeColor : "rgba(151,187,205,1)",
+			data : chartsData
+		}
+	]
+}
 //Get the context of the canvas element we want to select
 
-window.onload = function() {
+/*window.onload = function() {
 	var ctx = document.getElementById("myChart").getContext("2d");
 	var myNewChart = new Chart(ctx);
 	myNewChart.Radar(data);
-}
+
+	var ctx1 = document.getElementById("myChart1").getContext("2d");
+	var myNewChart = new Chart(ctx1);
+	myNewChart.Bar(data1);
+}*/
+$(function() {
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myNewChart = new Chart(ctx);
+	myNewChart.Radar(data);
+
+	var ctx1 = document.getElementById("myChart1").getContext("2d");
+	var myNewChart = new Chart(ctx1);
+	myNewChart.Bar(data1);
+
+	
+});
 
 
 
